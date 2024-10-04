d806bbc35ec3710edc34288e8b189fe6c971f926
edsonaraujoneto <edsonaraujodesouzaneto@gmail.com>
2024-10-03 21:23:40 -0300

    feat: create new mutation to user

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,11 @@
 import { ApolloServer, gql } from "apollo-server";
-import { createUser } from "./routes/create-user.js";
+import { resolvers } from "./resolvers.js";
+import { readFileSync } from "fs";
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => "Hello world!",
-  },
-};
+const typeDefs = gql(readFileSync("./src/schema.graphql", "utf-8"));
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-const testCreateUser = async () => {
-  try {
-    const newUser = await createUser();
-
-    if (newUser) {
-      console.log("Usuário criado com sucesso");
-    } else {
-      console.log("Usuário não criado");
-    }
-  } catch (error) {
-    console.log("Erro ao criar usuário:", error);
-  }
-};
-
 server.listen().then(async ({ url }) => {
   console.log(`Servidor pronto em ${url}`);
-  testCreateUser();
 });
