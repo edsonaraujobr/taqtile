5ea0e7678c37fe05c17faf2f0357d3f72d3857d7
edsonaraujoneto <edsonaraujodesouzaneto@gmail.com>
2025-01-09 10:06:54 -0300

    feature: create login to user with authentication

--- src/graphql/schemas/userSchema.ts
+++ src/graphql/schemas/userSchema.ts
@@ -1,14 +1,22 @@
 import { gql } from "apollo-server";
 
 export const userSchema = gql`
+<<<<<<< HEAD
   input UserCreateInput {
+=======
+  input createUser {
+>>>>>>> 2fe3f63 (feature: create login to user with authentication)
     name: String
     email: String!
     password: String!
     birthDate: String
   }
 
+<<<<<<< HEAD
   input UserLoginInput {
+=======
+  input UserLogin {
+>>>>>>> 2fe3f63 (feature: create login to user with authentication)
     email: String!
     password: String!
   }
