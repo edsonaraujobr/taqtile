1669b37ef890ddba61af6e9565a4f9ede48787d3
edsonaraujoneto <edsonaraujodesouzaneto@gmail.com>
2025-01-15 10:13:32 -0300

    update test login with authentication

--- test/integration/user/loginTest.ts
+++ test/integration/user/loginTest.ts
@@ -114,12 +114,17 @@ describe("User Mutation - Teste de Login", () => {
     const loginUser = responseLogin.data.data.loginUser.user;
     expect(loginUser).to.have.property("id");
 <<<<<<< HEAD
+<<<<<<< HEAD
     expect(loginUser.name).to.equal(validUser.name);
     expect(loginUser.email).to.equal(validUser.email);
 =======
     expect(loginUser.name).to.equal(name);
     expect(loginUser.email).to.equal(email);
 >>>>>>> f8d8cac (feat: create remember-me to user login)
+=======
+    expect(loginUser.name).to.equal(validUser.name);
+    expect(loginUser.email).to.equal(validUser.email);
+>>>>>>> 815b0e3 (update test login with authentication)
   });
 
   afterEach(async () => {
