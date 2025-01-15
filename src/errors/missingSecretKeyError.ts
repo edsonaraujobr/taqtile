52ef9e4864094db46b4ba3d7eb2d2da726f4cb55
edsonaraujoneto <edsonaraujodesouzaneto@gmail.com>
2025-01-15 08:05:17 -0300

    feat: create if to verification SECRET_KEY

---
+++ src/errors/missingSecretKeyError.ts
@@ -0,0 +1,18 @@
+import { CustomError } from "./customError.js";
+
+export class MissingSecretKeyError extends CustomError {
+  constructor({
+    message = "SECRET_KEY não está definido no arquivo .env",
+    additionalInfo,
+  }: {
+    message?: string;
+    additionalInfo?: string;
+  } = {}) {
+    super({
+      code: 500,
+      message,
+      additionalInfo,
+    });
+    this.name = "MissingSecretKeyError";
+  }
+}
--- src/middlewares/authenticateUser.ts
+++ src/middlewares/authenticateUser.ts
@@ -1,22 +1,17 @@
 import { AuthenticationError } from "apollo-server";
 import { JwtService } from "../services/jwtService.js";
-import { BadInputError } from "../errors/badInputError.js";
 
 export const authenticate = (token) => {
-  if (!token) {
+  if (!token || !token.startsWith("Bearer ")) {
     throw new AuthenticationError(
-      "Para continuar, você deve fornecer um token de validação válido!",
+      "Para continuar, você deve fornecer um token de validação válido no formato 'Bearer <token>'!"
     );
-  }
-
-  if (!token) {
-    throw new AuthenticationError("Formato do token de validação inválido! ");
   }
 
   try {
     const decoded = JwtService.verifyToken(token.replace("Bearer ", ""));
-    return decoded; 
+    return decoded;
   } catch (error) {
-    throw new AuthenticationError("Token de validação expirado!");
+    throw new AuthenticationError("Token de validação expirado ou inválido!");
   }
 };
