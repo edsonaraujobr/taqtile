96206fb501492577f46f5cb6597d87e2900cbf96
edsonaraujoneto <edsonaraujodesouzaneto@gmail.com>
2025-02-18 18:36:21 -0300

    fix: add user datasource

---
+++ src/data/user/user.db.datasource.ts
@@ -0,0 +1,59 @@
+import { database } from "../database/database.js";
+import { CreateUserModel, UserModel } from "../../domain/models/index.js";
+import { Service } from "typedi";
+
+@Service()
+export class UserDBDataSource {
+  private constructor() {}
+
+  async create({ data }: { data: CreateUserModel }): Promise<UserModel> {
+    return await database.user.create({
+      data,
+      include: { addresses: true },
+    });
+  }
+
+  async findByEmail({ email }: { email: string }): Promise<UserModel | null> {
+    return await database.user.findUnique({
+      where: { email },
+      include: {
+        addresses: true,
+      },
+    });
+  }
+
+  async findByID({ id }: { id: string }): Promise<UserModel | null> {
+    return await database.user.findUnique({
+      where: { id },
+      include: {
+        addresses: true,
+      },
+    });
+  }
+
+  async count(): Promise<number> {
+    return await database.user.count();
+  }
+
+  async findMany({
+    skip,
+    take,
+  }: {
+    skip: number;
+    take: number;
+  }): Promise<UserModel[]> {
+    return await database.user.findMany({
+      orderBy: { name: "asc" },
+      skip,
+      take,
+      where: {
+        email: {
+          not: "admin@admin.com",
+        },
+      },
+      include: {
+        addresses: true,
+      },
+    });
+  }
+}
